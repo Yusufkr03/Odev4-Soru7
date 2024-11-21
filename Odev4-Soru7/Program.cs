224df9827cbc5258f5295ade194139d7bfdb04fa
Yusufkr03 <yusufkr2903@gmail.com>
2024-11-21 22:00:10 +0300

    Add files via upload

---
+++ Odev4-Soru7/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program
+{
+    public static void Main()
+    {
+        
+        Console.WriteLine(metod.YasHesap());
+        Console.WriteLine(metod.YasHesap(20));
+        Console.WriteLine(metod.YasHesap(52));
+        Console.WriteLine(metod.YasHesap(15));
+    }
+}
+
+
+
+class metod
+{
+    public static int YasHesap(int y = 18)
+    {
+        if (y > 18)
+        {
+            int sonuc = y - 18;
+            return sonuc;
+        }
+        else return 0;
+        
+        
+        
+    }
+
+}
